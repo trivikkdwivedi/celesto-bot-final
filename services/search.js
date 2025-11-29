47a78d88a21ad7cf053443c68d7cb488f84a9486
trivikkdwivedi <trivikkdwivedi@gmail.com>
2025-11-29 23:57:54 +0530

    Update search.js

--- services/search.js
+++ services/search.js
@@ -1,1 +1,22 @@
+const axios = require("axios");
 
+async function searchTokens(query) {
+  try {
+    const url = `https://public-api.birdeye.so/defi/v3/search?query=${encodeURIComponent(query)}`;
+
+    const res = await axios.get(url, {
+      headers: {
+        Accept: "application/json",
+        "X-API-KEY": process.env.BIRDEYE_API_KEY
+      },
+      timeout: 5000
+    });
+
+    return res.data?.data?.tokens || [];
+  } catch (err) {
+    console.error("Token search error:", err.message);
+    return [];
+  }
+}
+
+module.exports = { searchTokens };
