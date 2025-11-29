e4ee4f74396a3c5e8b89ed57e9d194fd16511753
trivikkdwivedi <trivikkdwivedi@gmail.com>
2025-11-29 23:18:48 +0530

    Update menu.js

--- handlers/menu.js
+++ handlers/menu.js
@@ -1,1 +1,27 @@
+const { Markup } = require("telegraf");
 
+module.exports = async (ctx) => {
+  return ctx.reply(
+    "📍 *Main Menu*",
+    {
+      parse_mode: "Markdown",
+      reply_markup: {
+        inline_keyboard: [
+          [
+            { text: "💰 Price", callback_data: "menu_price" },
+            { text: "📘 Info", callback_data: "menu_info" }
+          ],
+          [
+            { text: "🧩 Create Wallet", callback_data: "menu_createwallet" }
+          ],
+          [
+            { text: "🔑 My Wallet", callback_data: "menu_mywallet" }
+          ],
+          [
+            { text: "💵 Balance", callback_data: "menu_balance" }
+          ]
+        ]
+      }
+    }
+  );
+};
